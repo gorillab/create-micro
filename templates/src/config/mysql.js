ba9f5007f3c0e1ad6971994df636ceef6b8379c1
Khang Huynh <khanghuynh92@gmail.com>
2017-12-05 20:27:17 +0700

    Refactor code

--- templates/src/config/mysql.js
+++ templates/src/config/mysql.js
@@ -1,19 +1,14 @@
 const Sequelize = require('sequelize');
 
-const sequelizeDB = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USERNAME,
-  process.env.DB_PASSWORD, {
-    host: process.env.HOST,
-    port: process.env.PORT,
-    dialect: 'mysql',
-    define: {
-      timestamps: false,
-    },
-    native: false,
-    logging: false,
-    timezone: '+07:00',
+const sequelizeDB = new Sequelize(`mysql://${process.env.MYSQL_URL}`, {
+  dialect: 'mysql',
+  define: {
+    timestamps: false,
   },
+  native: false,
+  logging: false,
+  timezone: '+07:00',
+},
 );
 
 const load = (name) => require(`../${name}.model.js`)(sequelizeDB, Sequelize);  // eslint-disable-line
